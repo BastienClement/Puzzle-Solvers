5cdc230c3e1bc41acc7dcee7bc9d6d4f2965904a
galedric <g@ledric.me>
2013-07-30 19:46:52 +0200

    Add Hashi puzzle solver

--- hashi.lua
+++ hashi.lua
@@ -25,133 +25,269 @@ local grid = {}
 local gridW, gridH = 0, 0
 
 local nodes = {}
-local links = {}
+
+local TOP, RIGHT, BOTTOM, LEFT = 1, 2, 3, 4
+local HORIZONTAL, VERTICAL = 1, 2
+
+local OPPOSITE_SIDE = {
+	[TOP] = BOTTOM,
+	[RIGHT] = LEFT,
+	[BOTTOM] = TOP,
+	[LEFT] = RIGHT
+}
+
+local SIDES = {
+	[TOP] = "TOP",
+	[RIGHT] = "RIGHT",
+	[BOTTOM] = "BOTTOM",
+	[LEFT] = "LEFT"
+}
+
+local ORIENTATIONS = {
+	[HORIZONTAL] = "HORIZONTAL",
+	[VERTICAL] = "VERTICAL"
+}
+
+local print_grid
+
+-- Objects
+
+local node_mt = {}
+node_mt.__index = node_mt
+
+function node_mt:UpdateCount()
+	self.count = 0
+	
+	for side, link in pairs(self.links) do
+		self.count = self.count + link.count
+	end
+	
+	if not self.complete and self.count == self.value then
+		self.complete = true
+		
+		for side, link in pairs(self.links) do
+			if not link.complete then
+				self:SetLink(side, link.count, true)
+			end
+		end
+	end
+end
+
+function node_mt:Missing()
+	return self.value - self.count
+end
+
+function node_mt:SetLinkPossible(side, possible)
+	local link = self.links[side]
+	assert(link, "link is undefined")
+	assert(not link.complete, "link is already competed")
+	
+	if possible == link.possible then
+		return false
+	end
+	
+	assert(link.count <= possible)
+	assert(possible <= link.possible, "attempting to assign a greater possible value ")
+	link.possible = possible
+	
+	if possible == link.count then
+		self:SetLink(side, possible, true)
+	else
+		link.a:UpdateCount()
+		link.b:UpdateCount()
+	end
+	
+	return true
+end
+
+function node_mt:SetLink(side, count, exact)
+	local link = self.links[side]
+	assert(link, "link is undefined")
+	assert(not link.complete, "link is already competed")
+	
+	if not exact and count == link.count then
+		return false
+	end
+	
+	assert(count <= link.possible)
+	assert(count >= link.count, "attempting to assign a lesser count value ")
+	link.count = count
+	
+	link.a:UpdateCount()
+	link.b:UpdateCount()
+	
+	if count > 0 then
+		for i, link in pairs(link.crossing) do
+			assert(not link.complete or link.count == 0, "a crossing link is already completed")
+		
+			link.count = 0
+			link.possible = 0
+			link.complete = true
+			
+			link.a:UpdateCount()
+			link.b:UpdateCount()
+		end
+	end
+	
+	if exact or link.count == link.possible then
+		link.possible = count
+		link.complete = true
+	end
+	
+	return true
+end
 
 -- Parse
 
 do
-	local node_mt = {}
-	node_mt.__index = node_mt
-	
-	function node_mt:Next(i)
-		local link = self.links[i]
-		return (link.a == self) and b or a
-	end
-	
-	function node_mt:NextValue(i)
-		local node = self:Next(i)
-		return node and node.value or 0
-	end
-	
-	function node_mt:Sum()
-		local sum = 0
-		for _, link in ipairs(self.links) do
-			sum = sum + link.type
-		end
-		return sum
-	end
-	
-	local function makeNode(r, c, value)
-		local v = tonumber(value)
-		if not v then return end
+	local function makeLink(a, b, orientation)
+		local link = {
+			a = a,
+			b = b,
+			count = 0,
+			possible = 2,
+			complete = false,
+			crossing = {}
+		}
 		
+		local aSide = (orientation == VERTICAL) and BOTTOM or RIGHT
+		local bSide = (orientation == VERTICAL) and TOP or LEFT
+		
+		a.links[aSide] = link
+		b.links[bSide] = link
+		
+		a.linksNode[aSide] = b
+		b.linksNode[bSide] = a
+		
+		a.linksCount = a.linksCount + 1
+		b.linksCount = b.linksCount + 1
+		
+		return link
+	end
+	
+	-- Look for a node on the top of the current node
+	local function traceUp(r, c)
+		r = r - 1
+		if r < 1 then
+			return false
+		elseif grid[r][c] and grid[r][c].node then
+			return grid[r][c]
+		else
+			return traceUp(r, c)
+		end
+	end
+	
+	-- Look for a node on the left of the current node
+	local function traceLeft(r, c)
+		c = c - 1
+		if c < 1 then
+			return false
+		elseif grid[r][c] and grid[r][c].node then
+			return grid[r][c]
+		else
+			return traceLeft(r, c)
+		end
+	end
+	
+	-- Look for nodes around the current node
+	local function trace(r, c, node)
+		local up = traceUp(r, c)
+		if up then
+			local link = makeLink(up, node, VERTICAL)
+			
+			for rr = r - 1, up.row + 1, -1 do
+				local cell = grid[rr][c]
+				if not cell then
+					grid[rr][c] = {
+						links = true,
+						[VERTICAL] = link
+					}
+				elseif not cell.node then
+					local cross = grid[rr][c][HORIZONTAL]
+					if cross then
+						cross.crossing[#cross.crossing + 1] = link
+						link.crossing[#link.crossing + 1] = cross
+					end
+					grid[rr][c][VERTICAL] = link
+				end
+			end
+		end
+		
+		local left = traceLeft(r, c)
+		if left then
+			local link = makeLink(left, node, HORIZONTAL)
+			
+			for cc = c - 1, left.col + 1, -1 do
+				local cell = grid[r][cc]
+				if not cell then
+					grid[r][cc] = {
+						links = true,
+						[HORIZONTAL] = link
+					}
+				end
+			end
+		end
+	end
+	
+	-- Create a node object
+	local function makeNode(r, c, v)
 		local node = {
 			node = true,
-			done = false,
+			complete = false,
 			value = v,
+			count = 0,
 			row = r,
 			col = c,
-			links = {}
+			links = {},
+			linksNode = {},
+			linksCount = 0
 		}
 		
 		setmetatable(node, node_mt)
-	
+		trace(r, c, node)
+		
 		nodes[#nodes + 1] = node
 		return node
 	end
 	
-	local r = 1
-	for line in io.lines() do
-		if r > gridH then
-			gridH = r
-		end
-	
-		grid[r] = {}
-	
-		local c = 1
-		for char in line:gmatch(".") do
-			if c > gridW then
-				gridW = c
+	-- Parse input
+	local function parse()
+		local r = 1
+		for line in io.lines() do
+			if r > gridH then
+				gridH = r
 			end
 		
-			grid[r][c] = makeNode(r, c, char)
-			c = c + 1
-		end
-	
-		r = r + 1
-	end
-end
-
--- Linking
-
-do
-	local function link(a, aSide, b, bSide)
-		local o = bSide == "top" and "vertical" or "horizontal"
+			grid[r] = {}
 		
-		local link = {
-			link = true,
-			type = 0,
-			a = a,
-			b = b,
-			o = o
-		}
-		
-		if o == "vertical" then
-			local c = a.col
-				for r = a.row + 1, b.row - 1 do
-				grid[r][c] = link
-			end
-		else
-			local r = a.row
-			for c = a.col + 1, b.col - 1 do
-				grid[r][c] = link
-			end
-		end
-		
-		a[aSide] = link
-		b[bSide] = link
-		
-		a.links[#a.links + 1] = link
-		b.links[#b.links + 1] = link
-		
-		links[#links + 1] = links
-	end
-	
-	for r = 1, gridH do
-		for c = 1, gridW do
-			local cell = grid[r][c]
-			if cell and cell.node then
-				for rr = r + 1, gridH do
-					if grid[rr][c] and grid[rr][c].node then
-						link(cell, "bottom", grid[rr][c], "top")
-						break
-					end
+			local c = 1
+			for char in line:gmatch(".") do
+				if char == "." then
+					return
 				end
 			
-				for cc = c + 1, gridW do
-					if grid[r][cc]  and grid[r][cc].node then
-						link(cell, "right", grid[r][cc], "left")
-						break
-					end
+				local v = tonumber(char)
+				
+				if c > gridW then
+					gridW = c
 				end
+				
+				if v and v > 0 and v < 9 then
+					grid[r][c] = makeNode(r, c, v)
+				end
+			
+				c = c + 1
 			end
+		
+			r = r + 1
 		end
 	end
+	
+	parse()
 end
 
 -- Print
 
-local print_grid
 do
 	local circled = {
 		[1] = "①",
@@ -165,37 +301,30 @@ do
 	}
 	
 	function print_grid()
-		local out_grid = {}
-		
 		for r = 1, gridH do
-			out_grid[r] = {}
-		
 			for c = 1, gridW do
 				local cell = grid[r][c]
-				if cell and cell.node then
-						out_grid[r][c] = " " .. cell.value .. " "
-				elseif cell and cell.link and cell.type ~= 0 then
-					if cell.o == "vertical" then
-						if cell.type == 1 then
-							out_grid[r][c] = " │ "
+				if cell then
+					if cell.node then
+						io.write(cell.complete and "[" or " ", cell.value, cell.complete and "]" or " ")
+					elseif cell.links then
+						local h = cell[HORIZONTAL]
+						local v = cell[VERTICAL]
+						
+						if h and h.count > 0 then
+							io.write(h.count == 2 and "═══" or "───")
+						elseif v and v.count > 0 then
+							io.write(v.count == 2 and " ║ " or " │ ")
 						else
-							out_grid[r][c] = " ║ "
-						end
-					else
-						if cell.type == 1 then
-							out_grid[r][c] = "───"
-						else
-							out_grid[r][c] = "═══"
+							io.write("   ")
 						end
 					end
 				else
-					out_grid[r][c] = "   "
+					io.write("   ")
 				end
 			end
+			print()
 		end
-	
-		print()
-		for i = 1, #out_grid do print(table.concat(out_grid[i], "")) end
 		print()
 	end
 end
@@ -203,60 +332,123 @@ end
 print "Input:"
 print_grid()
 
---[[
+-- Solving
+
 do
+	print "Solving..."
 	local change_made = true
 	
-	local function set_link(link, type)
-		if link.type ~= type then
-			link.type = type
+	local function set_link_possible(node, side, possible)
+		if node:SetLinkPossible(side, possible) then
 			change_made = true
 		end
 	end
 	
+	local function set_link(node, side, count, exact)
+		if node:SetLink(side, count, exact) then
+			change_made = true
+		end
+	end
+
 	while change_made do
 		change_made = false
 		
-		for _, node in ipairs(nodes) do
-			if not node.done then
-				if node:Sum() == node.value then
-					node.done = true
-				else
-					if #node.links == 1 then
-						set_link(node.links[1], node.value)
-						node.done = true
-					end
-					
-					local possible_links = 0
-					for i = 1, #node.links do
-						if node:NextValue(i) == 1 and node.value == 1 then
-							-- Impossible
-						elseif node.value == 1 or node:NextValue(i) == 1 or (node:NextValue(i) == 2 and node.value == 2) then
-							possible_links = possible_links + 1
-						else
-							possible_links = possible_links + 2
+		for _, node in next, nodes do
+			if not node.complete then
+				local value = node.value
+				
+				-- Node with 1 missing link can only be linked once more
+				if node:Missing() == 1 then
+					for side, link in pairs(node.links) do
+						if not link.complete and link.count == 0 and link.possible > 1 then
+							set_link_possible(node, side, 1)
 						end
 					end
+				end
+				
+				-- A node has only the exact link count available
+				if not node.complete then
+					local possible = 0
+					for side, link in pairs(node.links) do
+						possible = possible + link.possible
+					end
 					
-					if possible_links == node.value then
-						for i, link in ipairs(node.links) do
-							if (node:NextValue(i) == 1 and node.value ~= 1) or (node:NextValue(i) == 2 and node.value == 2) then
-								set_link(link, 1)
-							else
-								set_link(link, 2)
+					if possible == node.value then
+						for side, link in pairs(node.links) do
+							if not link.complete then
+								set_link(node, side, link.possible, true)
 							end
 						end
-					elseif possible_links == node.value + 1 then
-						for i, link in ipairs(node.links) do
-							set_link(link, 1)
+					end
+				end
+				
+				-- Find required sides
+				if not node.complete then
+					local missing = node:Missing()
+					
+					for excluded, link in pairs(node.links) do
+						if not link.complete then
+							local remaining = missing
+						
+							for side, link in pairs(node.links) do
+								if side ~= excluded then
+									remaining = remaining - link.possible
+								end
+							end
+						
+							if remaining > 0 then
+								set_link(node, excluded, link.count + remaining)
+							end
+						end
+					end
+				end
+				
+				-- Closed circuits
+				if not node.complete then
+					local missing = node:Missing()
+					
+					if missing == 1 or missing == 2 then
+						for side, link in pairs(node.links) do
+							local n = node.linksNode[side]
+							if n:Missing() == missing and (not link.complete) and link.possible == missing then
+								local walked = { [node] = true, [n] = true }
+								local walkedCount = 2
+					
+								local function walk(node)
+									for side, n in pairs(node.linksNode) do
+										if node.links[side].count > 0 and not walked[n] then
+											if n.complete then
+												walked[n] = true
+												walkedCount = walkedCount + 1
+									
+												if not walk(n) then
+													return false
+												end
+											else
+												return false
+											end
+										end
+									end
+						
+									return true
+								end
+								
+								if walk(node) and walk(n) and walkedCount ~= #nodes then
+									set_link_possible(node, side, link.possible - 1)
+								end
+							end
 						end
 					end
 				end
 			end
 		end
 	end
+	
+	print_grid()
+end
+
+--[[
+for side, link in pairs(grid[6][1].links) do
+	print(SIDES[side], link.count, link.possible, link.complete)
 end
 ]]
-
-print "Solved:"
-print_grid()
